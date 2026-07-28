c00e997d2a659db60acc5dea59ae5428974b2bb5
prakash <prakashkalsariya1999@gmail.com>
2026-07-28 12:14:09 +0530

    some changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import { PlusCircle, LogOut } from "lucide-react";
 import Logo from "./Logo";
 import { Link } from "react-router-dom";
-import { AuthHelpers } from "../utils/auth.helpers";
 
 const Header = () => {
   return (
     <header className="bg-white shadow-md border-b sticky top-0 z-50">
       <div className="px-6 py-4 flex items-center justify-between cursor-pointer">
         <Logo />
+
         <nav className="hidden md:flex items-center gap-6">
           <Link
             to="/add-task"
